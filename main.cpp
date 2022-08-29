48bd89ab081e063fda682a044f99b06aa1f9cb8f
Neo_yoyo <bluecloud9508@gmail.com>
2022-08-29 12:02:41 +1000

    compressor

--- main.cpp
+++ main.cpp
@@ -4,34 +4,62 @@
 #include <iostream>
 #include <filesystem>
 
+#include <zstd.h>
+
 typedef unsigned char BYTE;
 
-int main()
-{
+int main(int argc, const char** argv) {
+
 
     std::string path_in = "/home/neo_yoyo/Документы/trash/in";
     std::string path_out = "/home/neo_yoyo/Документы/trash/out";
 
 
+//    for (int i = 0; i < argc; ++i) {
+//        std::cout << argv[i] << std::endl;
+//    }
+
+
     for (const auto & entry : std::filesystem::directory_iterator(path_in)) {
         std::cout << entry.path() << std::endl;
 
-        std::string filename = entry.path().string();
+        std::string in_filename = entry.path().string();
 
-        // open the file:
-        std::streampos fileSize = entry.file_size();
-        std::ifstream file(filename, std::ios::binary);
+        /// open the file:
+        std::streampos in_file_size = entry.file_size();
+        std::ifstream in_file(in_filename, std::ios::binary);
 
-        // read the data:
-        std::vector<BYTE> fileData(fileSize);
-        file.read((char*) &fileData[0], fileSize);
+        /// read the data:
+        std::vector<BYTE> in_buff(in_file_size);
+        in_file.read((char*) &in_buff[0], in_file_size);
 
+        in_file.close();
 
-        std::string filename_out = path_out + "/" + entry.path().filename().string() + ".arch";
+        /// вычисляем мах возможный размер выходного буфера,
+        /// чтобы не выйти за область памяти
+        size_t const out_buff_size = ZSTD_compressBound(in_file_size);
+        /// выделяем место в выходном буфере
+        std::vector<BYTE> out_buff(out_buff_size);
 
-        std::ofstream file_out(filename_out);
+        /// Сжатие
+        /// If you are doing many compressions, you may want to reuse the context.
+        /// See the multiple_simple_compression.c example.
+        size_t const out_file_size = ZSTD_compress((void*) &out_buff[0], out_buff_size, (void*) &in_buff[0], in_file_size, 16);
+        if (ZSTD_isError(out_file_size)) {
+            std::cout << ZSTD_getErrorName(out_file_size) << std::endl;
+            continue;
+        }
 
-        file_out.write((char*) &fileData[0], fileSize);
+        /// формируем имя выходного файла
+        std::string out_filename = path_out + "/" + entry.path().filename().string() + ".arch";
+
+        /// открываем выходной файл
+        std::ofstream out_file(out_filename);
+
+        /// записываем выходной файл
+        out_file.write((char*) &out_buff[0], out_file_size);
+
+        out_file.close();
     }
 
 
